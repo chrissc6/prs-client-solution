cef2ed4e2c9fc435b603781db1abb45e359978fa
Chris Caudill <christopherscaudill@gmail.com>
2019-03-15 11:24:59 -0400

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//user
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserCreateComponent } from './user/user-create/user-create.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'}, //1st route always
+  {path: 'users/list', component: UserListComponent},
+  {path: 'users/detail/:id', component: UserDetailComponent}, //id needed
+  {path: 'users/create', component: UserCreateComponent},
+  {path: 'users/edit/:id', component: UserEditComponent}, //id needed
+  {path: '**', component: HomeComponent} //last route always
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserCreateComponent } from './user/user-create/user-create.component';
