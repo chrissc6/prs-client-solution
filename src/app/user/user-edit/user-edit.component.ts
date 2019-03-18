d35c438a9bd518a31a813e2a02ec6d8cdedab978
Chris Caudill <christopherscaudill@gmail.com>
2019-03-18 18:27:10 -0400

    create edit page

--- src/app/user/user-edit/user-edit.component.ts
+++ src/app/user/user-edit/user-edit.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import{Router} from '@angular/router';
+import{ActivatedRoute} from '@angular/router';
+import {UserService} from '../user.service';
+import {User} from '../user.class';
+
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User("", "", "", "", "", "");
+
+  save():void{
+    this.userscvr.change(this.user)
+    .subscribe(
+      respond => { //success
+        console.log(respond);
+        this.router.navigateByUrl('/user/list');
+      },
+      err =>{ //error
+        console.error(err);
+      }
+    );
+  }
+
+  constructor(private userscvr: UserService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.params.id;
+
+    this.userscvr.get(id)
+      .subscribe(respond => {
+        console.log(respond);
+        this.user = respond;
+        });
   }
 
 }
