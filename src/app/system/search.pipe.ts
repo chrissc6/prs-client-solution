380506a53e703b8a89ee4e879a0eb8589fb3b4a5
Chris Caudill <christopherscaudill@gmail.com>
2019-03-19 16:21:12 -0400

    search working

--- src/app/system/search.pipe.ts
+++ src/app/system/search.pipe.ts
@@ -1,12 +1,56 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { User } from '../user/user.class';
+
 
 @Pipe({
   name: 'search'
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  //pass in arr of user
+  transform(users: User[], criteria: string): User[] {
+    //if empty return everything
+    if(criteria == "")
+    {
+      return users;
+    }
+
+    //create new array to return subset
+    let arrOut: User[] = [];
+    
+    //look a each user
+    for(let u of users)
+    {
+      if(u.username.toLowerCase().includes(criteria.toLowerCase()))
+      {
+        arrOut.push(u);
+        continue;
+      }
+      if(u.firstname.toLowerCase().includes(criteria.toLowerCase()))
+      {
+        arrOut.push(u);
+        continue;
+      }
+      if(u.lastname.toLowerCase().includes(criteria.toLowerCase()))
+      {
+        arrOut.push(u);
+        continue;
+      }
+      // u.phone.replace("-", "");
+      if(u.phone.toLowerCase().includes(criteria.toLowerCase()))
+      {
+        arrOut.push(u);
+        continue;
+      }
+      if(u.email.toLowerCase().includes(criteria.toLowerCase()))
+      {
+        arrOut.push(u);
+        continue;
+      }
+    }
+
+    return arrOut;
   }
 
 }
+
