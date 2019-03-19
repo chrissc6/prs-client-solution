d81cddfa835bdcc7a0ba7938de52f7a4c13fbe9c
Chris Caudill <christopherscaudill@gmail.com>
2019-03-19 15:38:41 -0400

    updated pipe

--- src/app/system/booldisp.pipe.ts
+++ src/app/system/booldisp.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class BooldispPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: boolean): string {
+    return value ? "Yes" : "No";
   }
 
 }
