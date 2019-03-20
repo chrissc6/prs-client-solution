fab947bed8422dc1771310de4b7432ee28a15d78
Chris Caudill <christopherscaudill@gmail.com>
2019-03-20 11:32:52 -0400

    updated pro list

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import{Product} from '../product.class';
+import{ProductService} from '../product.service';
+import {SystemService} from '../../system/system.service';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
+  searchCriteria: string = "";
+
+  constructor(private prosrvc: ProductService,
+    private syssvc: SystemService) { }
 
   ngOnInit() {
+    this.prosrvc.list()
+      .subscribe(respond => {
+        console.log(respond);
+        this.products = respond;
+      });
   }
 
 }
