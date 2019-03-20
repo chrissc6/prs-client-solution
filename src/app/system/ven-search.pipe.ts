03b811b639893b9f61b9c5b532b0ee43ce642f08
Chris Caudill <christopherscaudill@gmail.com>
2019-03-20 10:25:14 -0400

    ven search working

--- src/app/system/ven-search.pipe.ts
+++ src/app/system/ven-search.pipe.ts
@@ -1,12 +1,63 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Vendor } from '../vendor/vendor.class';
 
 @Pipe({
   name: 'venSearch'
 })
 export class VenSearchPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(vendors: Vendor[], crit: string): Vendor[] {
+    if(crit == "")
+    {
+      return vendors;
+    }
+
+    let arrOut: Vendor[] = [];
+
+    for(let v of vendors)
+    {
+      if(v.code.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.name.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.address.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.city.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.state.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.zip.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.phone.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.email.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+    }
+    return arrOut;
   }
 
 }
