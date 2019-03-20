c42c8148480b28b3cf12078ec29634ecd0fb6b0f
Chris Caudill <christopherscaudill@gmail.com>
2019-03-20 12:32:54 -0400

    updated pro edit

--- src/app/product/product-edit/product-edit.component.ts
+++ src/app/product/product-edit/product-edit.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import{Router} from '@angular/router';
+import{ActivatedRoute} from '@angular/router';
+import{Product} from '../product.class';
+import{ProductService} from '../product.service';
+import {SystemService} from '../../system/system.service';
+
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
@@ -7,9 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductEditComponent implements OnInit {
 
-  constructor() { }
+  product: Product;
+
+  save():void{
+    this.proscvr.change(this.product)
+    .subscribe(
+      respond => { //success
+        console.log("Product Update Successful!: ", respond);
+        this.router.navigateByUrl('/product/list');
+      },
+      err =>{ //error
+        console.error("Product Update Error: ", err);
+      }
+    );
+  }
+
+  constructor(private proscvr: ProductService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private syssvc: SystemService) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.params.id;
+
+    this.proscvr.get(id)
+      .subscribe(
+        respond => {
+        console.log(respond);
+        this.product = respond;
+        },
+        err =>{
+          console.error(err);
+        }
+      );
   }
 
 }
