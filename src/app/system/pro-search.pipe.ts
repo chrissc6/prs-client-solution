81e1c6b5848049500583f0b75940f6f1bf4e1c02
Chris Caudill <christopherscaudill@gmail.com>
2019-03-20 20:10:32 -0400

    edited pro and re search

--- src/app/system/pro-search.pipe.ts
+++ src/app/system/pro-search.pipe.ts
@@ -1,12 +1,48 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import{Product} from '../product/product.class';
 
 @Pipe({
   name: 'proSearch'
 })
 export class ProSearchPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(products: Product[], crit: string): Product[] {
+    if(crit == "")
+    {
+      return products;
+    }
+
+    let arrOut: Product[] = [];
+
+    for(let v of products)
+    {
+      if(v.vendor.name.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.partNumber.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.name.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.unit.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      // if(v.photoPath.toLowerCase().includes(crit.toLowerCase()))
+      // {
+      //   arrOut.push(v);
+      //   continue;
+      // }
+    }
+    return arrOut;
   }
 
 }
--- src/app/system/re-search.pipe.ts
+++ src/app/system/re-search.pipe.ts
@@ -1,12 +1,48 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import{Request} from '../request/request.class';
 
 @Pipe({
   name: 'reSearch'
 })
 export class ReSearchPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(requests: Request[], crit: string): Request[] {
+    if(crit == "")
+    {
+      return requests;
+    }
+
+    let arrOut: Request[] = [];
+
+    for(let v of requests)
+    {
+      if(v.user.username.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.description.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.deliveryMode.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.submittedDate.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+      if(v.status.toLowerCase().includes(crit.toLowerCase()))
+      {
+        arrOut.push(v);
+        continue;
+      }
+    }
+    return arrOut;
   }
 
 }
